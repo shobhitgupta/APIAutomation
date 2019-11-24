a39c454c118b5c29f7a7221a0e59d64db3f24d1f
shogupta <shogupta@publicisgroupe.net>
2019-11-24 18:50:03 +0530

    adding libraries

--- src/main/java/services/BaseService.java
+++ src/main/java/services/BaseService.java
@@ -1,5 +1,10 @@
 package services;
 
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.* ;
+import static  io.restassured.matcher.RestAssuredMatchers.* ;
+import static   org.hamcrest.Matchers.*;
 public  abstract class BaseService {
 
 
